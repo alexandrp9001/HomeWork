66dc4f267f924c72372d57c7eb2b714f68945946
alexandrp9001 <140640940+alexandrp9001@users.noreply.github.com>
2023-08-09 23:05:09 +0300

    Задача 10 и 13 выполнены

---
+++ task10/Program.cs
@@ -0,0 +1,8 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+int randomNumber = new Random().Next(100, 1000);
+System.Console.WriteLine("Выбрвно число:");
+System.Console.WriteLine(randomNumber);
+int ost = randomNumber%100;
+int center = ost/10;
+System.Console.WriteLine("Второе число трехзначного числа:");
+System.Console.WriteLine(center);
---
+++ task13/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.WriteLine("Введите число:");
+int num = int.Parse(Console.ReadLine());
+int result = -1;
+if (num >= 100)
+
+{
+   while (num > 1000)
+   {
+    num = num / 10;
+   }
+   result = num % 10;
+
+    System.Console.WriteLine("Третья цифра заданного числа:");
+    System.Console.WriteLine(result);
+    return;
+}
+else
+{
+    System.Console.WriteLine( "В заданном числе меньше трех цифр");
+}
